264f687f84391b68d52ddd33932b3375d6fbca01
Rozmus <arozmus@deloittece.com>
2022-07-29 10:50:14 +0200

    Add files

---
+++ cypress/e2e/page_objects/main-page.js
@@ -0,0 +1,27 @@
+export class MainPage {
+
+   
+
+    static openAutomationPracticePage(){
+
+        cy.visit("http://automationpractice.com/index.php");
+
+    }
+
+
+
+    static clickCategory(name){
+
+        cy.get('#block_top_menu').contains(name).click();
+
+    }
+
+
+
+    static clickShoppingCart(){
+
+        cy.get('.shopping_cart').contains("Cart").click();
+
+    }
+
+}
